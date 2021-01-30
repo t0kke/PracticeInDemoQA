2e0fced1dae332f206bf3e4b0c638389902ad71d
German <ger.kataev@gmail.com>
2021-01-30 22:28:34 +0300

    Add filling the calendar

--- src/test/java/StudentRegistrationFormTest.java
+++ src/test/java/StudentRegistrationFormTest.java
@@ -1,23 +1,26 @@
+import config.BaseTest;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.*;
 
-public class StudentRegistrationFormTest {
+public class StudentRegistrationFormTest extends BaseTest {
     @Test
-    void checkingTheFormFilling() {
+    void checkingTheFormFilling () {
         String firstName = "German";
         String lastName = "Kataev";
         String email = "test@test.tt";
-        String number = "89998887766";
-        String sex  = "Male";
+        String phoneNumber = "89998887766";
+        String gender  = "Male";
+        String date = "16 Jul 1993";
 
         open("https://demoqa.com/automation-practice-form");
         $("#firstName").setValue(firstName);
         $("#lastName").setValue(lastName);
         $("#userEmail").setValue(email);
+        $x("//label[text()='" + gender + "']").click();
+        $("#userNumber").setValue(phoneNumber);
+        executeJavaScript(String.format("$('[id=dateOfBirthInput]').val('%s')", date));
 
-        $x("//label[text()='" + sex + "']").click();
-        $("#userNumber").setValue(number);
 
 
 
