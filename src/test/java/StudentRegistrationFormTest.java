637936061fb01d1a5d3fa39e1da2bf2e0c7f3ed1
German <ger.kataev@gmail.com>
2021-01-31 15:08:21 +0300

    Completed form filling and added form validation

--- src/test/java/StudentRegistrationFormTest.java
+++ src/test/java/StudentRegistrationFormTest.java
@@ -1,17 +1,32 @@
+import com.codeborne.selenide.Condition;
 import config.BaseTest;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.CollectionCondition.exactTexts;
 
 public class StudentRegistrationFormTest extends BaseTest {
     @Test
     void checkingTheFormFilling () {
-        String firstName = "German";
-        String lastName = "Kataev";
-        String email = "test@test.tt";
-        String phoneNumber = "89998887766";
-        String gender  = "Male";
-        String date = "16 Jul 1993";
+        String
+                firstName = "German",
+                lastName = "Kataev",
+                email = "test@test.tt",
+                phoneNumber = "9998887766",
+                gender  = "Male",
+                day = "16",
+                mounth = "July",
+                year = "1993",
+                subjects = "Arts",
+                hobbies = "Reading",
+                picName = "pug.jpeg",
+                currentAddress = "Prostokvashino",
+                state = "NCR",
+                city = "Noida";
+
 
         open("https://demoqa.com/automation-practice-form");
         $("#firstName").setValue(firstName);
@@ -19,7 +34,30 @@ public class StudentRegistrationFormTest extends BaseTest {
         $("#userEmail").setValue(email);
         $x("//label[text()='" + gender + "']").click();
         $("#userNumber").setValue(phoneNumber);
-        executeJavaScript(String.format("$('[id=dateOfBirthInput]').val('%s')", date));
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").click();
+        $x("//option[contains(text(),'" + year + "')]").scrollTo().click();
+        $(".react-datepicker__month-select").click();
+        $x("//option[contains(text(),'" + mounth + "')]").click();
+        $$(".react-datepicker__day").find(exactText("16")).click();
+        //$$(".react-datepicker__month-select").find(exactText(mounth)).click();
+        //$$(".react-datepicker__day").find(exactText(day)).click();
+        // executeJavaScript(String.format("$('[id=dateOfBirthInput]').val('%s')", date));
+        $("#subjectsInput").setValue(subjects).pressEnter();
+        $x("//label[text()='" + hobbies + "']").click();
+        $("#uploadPicture").uploadFile(new File("src/test/resources/files/pug.jpeg"));
+        $("#currentAddress").setValue(currentAddress);
+        $("#react-select-3-input").setValue(state).pressEnter();
+        $("#react-select-4-input").setValue(city).pressEnter();
+        $("#submit").click();
+
+        $$x("//tbody//td[2]").shouldHave(exactTexts(firstName + " " + lastName, email, gender, phoneNumber,
+                day + " " + mounth + "," + year , subjects, hobbies, picName, currentAddress, state + " " + city));
+
+
+
+
+
 
 
 
