67c6fbdcb9b97733c2f78c9d9adbfb2b767a83de
German <k4tger@gmail.com>
2021-02-04 09:27:51 +0300

    Removed variables SelenideElements. Refactoring method setDataByCalendar

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -1,8 +1,5 @@
 package pages;
 
-import com.codeborne.selenide.ElementsCollection;
-import com.codeborne.selenide.SelenideElement;
-
 import java.io.File;
 
 import static com.codeborne.selenide.CollectionCondition.exactTexts;
@@ -16,87 +13,59 @@ public class RegistrationPage {
     }
 
     public RegistrationPage setValueInFieldWithPlaceholder(String value, String namePlaceholder) {
-        SelenideElement fieldWithPlaceholder = $x("//input[@placeholder='" + namePlaceholder + "']");
-
-        fieldWithPlaceholder.setValue(value);
+        $x("//input[@placeholder='" + namePlaceholder + "']").setValue(value);
         return this;
     }
 
-    public RegistrationPage clickByRadiobuttonWithName(String genderName){
-        SelenideElement genderRadiobutton = $x("//label[text()='" + genderName + "']");
-
-        genderRadiobutton.click();
+    public RegistrationPage clickByRadiobuttonWithName(String genderName) {
+        $x("//label[text()='" + genderName + "']").click();
         return this;
     }
 
-    public RegistrationPage setDataByCalendar(String day, String month, String year){
-        SelenideElement dataOfBirthInput = $("#dateOfBirthInput");
-        SelenideElement yearInput = $(".react-datepicker__year-select");
-        SelenideElement choiceOfYearWithList = $x("//option[contains(text(),'" + year + "')]");
-        SelenideElement monthInput = $(".react-datepicker__month-select");
-        SelenideElement choiceOfMonthWithList = $x("//option[contains(text(),'" + month + "')]");
-        ElementsCollection dayPicker =  $$(".react-datepicker__day");
-
-        dataOfBirthInput.click();
-        yearInput.click();
-        choiceOfYearWithList.click();
-        monthInput.click();
-        choiceOfMonthWithList.click();
-        dayPicker.find(exactText(day)).click();
+    public RegistrationPage setDataByCalendar(String day, String month, String year) {
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").selectOption(year);
+        $(".react-datepicker__month-select").selectOption(month);
+        $$(".react-datepicker__day").find(exactText(day)).click();
         return this;
     }
 
-    public RegistrationPage setSubjectWithName(String subject){
-        SelenideElement subjectInput = $("#subjectsInput");
-
-        subjectInput.val(subject).pressEnter();
+    public RegistrationPage setSubjectWithName(String subject) {
+        $("#subjectsInput").setValue(subject).pressEnter();
         return this;
     }
 
-    public RegistrationPage choiceOfHobbyWithName(String nameHobby){
-        SelenideElement checkboxHobby = $x("//label[text()='" + nameHobby + "']");
-
-        checkboxHobby.click();
+    public RegistrationPage choiceOfHobbyWithName(String nameHobby) {
+        $x("//label[text()='" + nameHobby + "']").click();
         return this;
     }
 
-    public RegistrationPage uploadPicture(String filename){
-        SelenideElement inputFile = $("#uploadPicture");
-
-        inputFile.uploadFile(new File(filename));
+    public RegistrationPage uploadPicture(String filename) {
+        $("#uploadPicture").uploadFile(new File(filename));
         return this;
     }
 
-    public RegistrationPage enterAddressinField(String address){
-        SelenideElement addressField = $("#currentAddress");
-
-        addressField.setValue(address);
+    public RegistrationPage enterAddressinField(String address) {
+        $("#currentAddress").setValue(address);
         return this;
     }
 
-    public RegistrationPage choiceFromDropdownListWithName(String HeadingDropdownList, String ItemName){
-        SelenideElement dropdownInput = $x("//div[text()='" + HeadingDropdownList + "']").scrollTo();
-        SelenideElement itemDropdownList = $x("//div[text()='" + ItemName + "']");
-        
-        dropdownInput.click();
-        itemDropdownList.click();
+    public RegistrationPage choiceFromDropdownListWithName(String HeadingDropdownList, String ItemName) {
+        $x("//div[text()='" + HeadingDropdownList + "']").scrollTo().click();
+        $x("//div[text()='" + ItemName + "']").click();
         return this;
     }
 
-    public RegistrationPage clickBySubmitButton(){
-        SelenideElement sunmitButton = $("#submit");
-
-        sunmitButton.click();
+    public RegistrationPage clickBySubmitButton() {
+        $("#submit").click();
         return this;
     }
 
     public RegistrationPage CheckRegistrationData(String firstName, String lastName, String email, String gender, String phoneNumber,
                                                   String day, String month, String year, String subject, String hobbie,
-                                                  String picName, String currentAddress, String state, String city){
-        ElementsCollection registrationData = $$x("//tbody//td[2]");
-
-        registrationData.shouldHave(exactTexts(firstName + " " + lastName, email, gender, phoneNumber,
-                day + " " + month + "," + year , subject, hobbie, picName, currentAddress, state + " " + city));
+                                                  String picName, String currentAddress, String state, String city) {
+        $$x("//tbody//td[2]").shouldHave(exactTexts(firstName + " " + lastName, email, gender, phoneNumber,
+                day + " " + month + "," + year, subject, hobbie, picName, currentAddress, state + " " + city));
         return this;
     }
 }
