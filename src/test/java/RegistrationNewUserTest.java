c085aa08654f019ab8c6dbc29bbab7817d4787bf
German <k4tger@gmail.com>
2021-02-07 21:24:05 +0300

    Add generating variables in test

--- src/test/java/RegistrationNewUserTest.java
+++ src/test/java/RegistrationNewUserTest.java
@@ -1,27 +1,38 @@
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
 
-import static pages.RegistrationPage.openUrl;
+import java.util.Locale;
 
-public class RegistrationNewUserTest  {
+import static pages.RegistrationPage.openUrl;
+import static utils.GenerationFakeData.faker;
+
+public class RegistrationNewUserTest {
+    Faker faker = new Faker(new Locale("ru-RU"));
+    String firstName = faker().name().firstName();
+    String lastName = faker().name().lastName();
+    String email = faker.internet().emailAddress("test");
+    String phone = faker.phoneNumber().subscriberNumber(10);
+    String fullAddress = faker.address().fullAddress();
+
     @Test
     void registrationNewUser() {
         openUrl("https://demoqa.com/automation-practice-form")
-                .setValueInFieldWithPlaceholder("German", "First Name")
-                .setValueInFieldWithPlaceholder("Kataev", "Last Name")
-                .setValueInFieldWithPlaceholder("test@test.tt", "name@example.com")
+                .setValueInFieldWithPlaceholder(firstName, "First Name")
+                .setValueInFieldWithPlaceholder(lastName, "Last Name")
+                .setValueInFieldWithPlaceholder(email, "name@example.com")
                 .clickByRadiobuttonWithName("Male")
-                .setValueInFieldWithPlaceholder("9998887711","Mobile Number")
+                .setValueInFieldWithPlaceholder(phone, "Mobile Number")
                 .setDataByCalendar("16", "July", "1993")
                 .setSubjectWithName("Arts")
                 .choiceOfHobbyWithName("Reading")
                 .uploadPicture("src/test/resources/files/pug.jpeg")
-                .enterAddressinField("Dovatora Ul., bld. 16, appt. 55")
+                .enterAddressinField(fullAddress)
                 .choiceFromDropdownListWithName("Select State", "NCR")
                 .choiceFromDropdownListWithName("Select City", "Noida")
                 .clickBySubmitButton()
-                .CheckRegistrationData("German", "Kataev", "test@test.tt", "Male",
-                        "9998887711","16", "July", "1993", "Arts", "Reading",
-                        "pug.jpeg","Dovatora Ul., bld. 16, appt. 55", "NCR", "Noida");
+                .CheckRegistrationData(firstName, lastName, email, "Male",
+                        phone, "16", "July", "1993", "Arts", "Reading",
+                        "pug.jpeg", fullAddress, "NCR", "Noida");
     }
 }
 
